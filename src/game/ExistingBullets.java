df7c16bb18f6b7abfe3c7932b80959284f7099cb
Victor Niu <niuvictor@gmail.com>
2015-02-14 15:58:35 -0600

    Revert "Revert "Bullets & co.""
    This reverts commit e816218808e0275725b7dc643af5b6e959ce367b.

--- src/game/ExistingBullets.java
+++ src/game/ExistingBullets.java
@@ -1,5 +1,14 @@
 package game;
 
-public class ExistingBullets {
+import org.newdawn.slick.particles.ParticleSystem;
 
+public class ExistingBullets extends ParticleSystem {
+	
+	public ExistingBullets() {
+		super("img/bullet.png");
+	}
+	
+	public ExistingBullets(String i) {
+		super(i);
+	}
 }
