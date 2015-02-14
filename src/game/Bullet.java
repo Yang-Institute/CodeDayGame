e816218808e0275725b7dc643af5b6e959ce367b
Victor Niu <niuvictor@gmail.com>
2015-02-14 15:58:24 -0600

    Revert "Bullets & co."
    This reverts commit 520fa8c204e0a05b70500ce03d43cfd14c80c740.

--- src/game/Bullet.java
+++ src/game/Bullet.java
@@ -1,32 +1,5 @@
 package game;
 
-import org.newdawn.slick.Image;
-import org.newdawn.slick.SlickException;
-import org.newdawn.slick.particles.Particle;
-import org.newdawn.slick.particles.ParticleSystem;
+public class Bullet {
 
-public class Bullet extends Particle{
-//	private int damage;
-
-	protected float velx;
-	protected float vely;
-	protected float x;
-	protected float y;
-	
-	public Bullet(int xPlace, int yPlace) {
-		super(new ExistingBullets());
-		velx = 0;
-		vely = 15;
-		x = xPlace;
-		y = yPlace;
-	}
-	
-	public Bullet(ExistingBullets i) {
-		super(i);
-	}
-	public void move() {
-		x += velx;
-		y += vely;
-	}
-	
 }
--- src/game/ExistingBullets.java
+++ src/game/ExistingBullets.java
@@ -1,14 +1,5 @@
 package game;
 
-import org.newdawn.slick.particles.ParticleSystem;
+public class ExistingBullets {
 
-public class ExistingBullets extends ParticleSystem {
-	
-	public ExistingBullets() {
-		super("img/bullet.png");
-	}
-	
-	public ExistingBullets(String i) {
-		super(i);
-	}
 }
