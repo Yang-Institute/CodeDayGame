44994cf3bf6f55a634f64662644f4e8528515bb0
LaCamisaNegra <wesleywang123@yahoo.com>
2015-02-14 19:38:47 -0600

    shooter

--- src/game/Game.java
+++ src/game/Game.java
@@ -26,9 +26,10 @@ public class Game extends BasicGame {
 	public static final int HEIGHT = 540;
 	public static final int FPS = 60;
 	public static final double VERSION = 0.01;
-
-	Player player;
+	public static int deg = 90;
 	
+	Player player;
+
 	public ArrayList<Bullet> bullets = new ArrayList<Bullet>();
 
 	public Game(String gamename) {
@@ -38,34 +39,34 @@ public class Game extends BasicGame {
 	@Override
 	public void init(GameContainer gc) throws SlickException {
 		player = new Player(400, 320, "img/tank.png", "img/bullet2.png");
-		
-	}
-	
 
+	}
 
 	@Override
 	public void update(GameContainer gc, int i) throws SlickException {
 
 		Input input = gc.getInput();
-		if (input.isMousePressed(Input.MOUSE_LEFT_BUTTON)){
-			
+		if (input.isMousePressed(Input.MOUSE_LEFT_BUTTON)) {
+
 			int bulletX = player.pos.x + 24;
 			int bulletY = player.pos.y;
 			int mouseX = input.getMouseX();
 			int mouseY = input.getMouseY();
+
 			double mod = 10 / Math.sqrt(Math.pow(mouseX - bulletX, 2)
 					+ Math.pow(mouseY - bulletY, 2));
-			System.out.println(mod);
-			try{
-				bullets.add(new Bullet(bulletX, bulletY, (int) (mod *( mouseX-bulletX)),
-						(int) (mod * (mouseY-bulletY)), "img/bullet2.png"));
-				System.out.println(bullets.get(bullets.size()-1).vel.toString());
-			}
-			catch (SlickException e){
+
+			try {
+				bullets.add(new Bullet(bulletX, bulletY,
+						(int) (mod * (mouseX - bulletX)),
+						(int) (mod * (mouseY - bulletY)), "img/bullet2.png"));
+
+			} catch (SlickException e) {
 				e.printStackTrace();
 			}
+
 		}
-		
+
 		if (input.isKeyDown(Input.KEY_W)) {
 
 			player.pos.y -= 2;
@@ -100,15 +101,35 @@ public class Game extends BasicGame {
 
 	@Override
 	public void render(GameContainer gc, Graphics g) throws SlickException {
+		Input input = gc.getInput();
+
+		int tankX = player.pos.x + 31;
+		int tankY = player.pos.y + 31;
+		int mouseX = input.getMouseX();
+		int mouseY = input.getMouseY();
+		double distance = (int) Math.sqrt(Math.pow(mouseX - tankX, 2)
+				+ Math.pow(mouseY - tankY, 2));
+
+		double radians;
+		if (mouseX > tankX) {
+			radians = Math.asin((tankY - mouseY) / distance);
+		} else {
+			radians = 3.14 / 2 + Math.acos((tankY - mouseY) / distance);
+		}
+		int degrees = (int) (180 / 3.14 * radians);
+		
+		player.img.rotate(deg - degrees);
+		
+		deg = degrees;
+
 		g.drawString("Health: " + player.health, 0, 520);
-		
+
 		player.img.draw(player.pos.x, player.pos.y);
-		
+
 		for (Bullet b : bullets) {
 			b.pos.x += b.vel.x;
 			b.pos.y += b.vel.y;
 			b.img.draw(b.pos.x, b.pos.y);
-
 
 		}
 		// g.drawString("Hello World", 50, 50);
@@ -127,6 +148,5 @@ public class Game extends BasicGame {
 			Logger.getLogger(Game.class.getName()).log(Level.SEVERE, null, ex);
 		}
 	}
-
 
 }
